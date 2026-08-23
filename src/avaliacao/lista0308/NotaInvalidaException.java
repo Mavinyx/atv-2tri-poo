e374626cbbe27ee45c8a86f694bd875b5b9c8ae2
Mavinyx <nicollyvianamartins@gmail.com>
2026-08-23 19:02:21 -0300

    adiciona a exception notainvalida e sua chamada

---
+++ src/avaliacao/lista0308/NotaInvalidaException.java
@@ -0,0 +1,7 @@
+package avaliacao.lista0308;
+
+public class NotaInvalidaException extends RuntimeException {
+    public NotaInvalidaException(String message) {
+        super(message);
+    }
+}
--- src/avaliacao/lista0308/Teste.java
+++ src/avaliacao/lista0308/Teste.java
@@ -4,9 +4,14 @@ import java.util.Arrays;
 
 public class Teste {
     public static void main(String[] args) {
-        Estudante reg1 = new Estudante("fulanito cabrito");
-        reg1.insereNotas();
-        System.out.println(Arrays.toString(reg1.getNotas()));
-        System.out.println("Média de "+reg1.getNomeEstudante()+":"+reg1.calculaMedia());
+        Estudante aluno = new Estudante("João");
+        try {
+            aluno.insereNotas();
+            System.out.println("Média: " + aluno.calculaMedia());
+        } catch (NotaInvalidaException e) {
+
+            System.out.println(e.getMessage());
+            System.out.println("Tente cadastrar o aluno novamente.");
+        }
     }
 }
